955456ae53371dddd0c2ff610d8014fc0e1e2606
sajeerpm <sajeerpm.mec@gmail.com>
2023-06-03 23:45:06 +0530

    fix 5

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
-import { ContextProvider } from "./context/ContextProvider";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
