ac36fbb44581b8906513bede836bc7cb8d7dab89
sajeerpm <sajeerpm.mec@gmail.com>
2023-06-04 08:38:44 +0530

    fix 14

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/laundary-web-2/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
