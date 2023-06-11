6c849ceaddecc1132d5a5559f52e805772373c09
Sajeer M <sajeer.m@freshtohome.com>
2023-06-11 13:42:56 +0530

    added latest changes - 11/6 - 2

--- src/pages/faq/faq.tsx
+++ src/pages/faq/faq.tsx
@@ -70,7 +70,7 @@ const FAQ: React.FC = () => {
         <div className="w-full py-20">
           <HText textAlign="text-center">FEQUENTLY ASKED QUESTIONS</HText>
         </div>
-        <div className="h-full px-4 py-8">
+        <div className="h-full py-8 md:px-4">
           <div className="mx-auto max-w-5xl">
             {faqItems.map((item) => (
               <div
